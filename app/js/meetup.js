91e17afee4e18d0a6f948545aed29d64290731d3
Marcus Bristol <marcus.bristol@rededge.co.nz>
2014-04-06 23:53:52 +1200

    Added random person name generator

--- app/js/meetup.js
+++ app/js/meetup.js
@@ -1,7 +1,28 @@
 // App.MeetupRoute = Ember.Route.extend(Ember.SimpleAuth.AuthenticatedRouteMixin, {
 // 	model: function() {
+// 		var explodeTheGuests = function(rsvps) {
+// 			var guests = rsvps.filter(function(rsvp) {
+// 				return rsvp.guests > 0;
+// 			});
+
+// 			guests.forEach(function(rsvp) {
+// 				for (var guest = 0; guest < rsvp.guests; guest++) {
+// 					rsvps.push({
+// 						member: {
+// 							name: rsvp.member.name + ' (Guest #' + (guest + 1) + ')',
+// 							member_id: rsvp.member_id
+// 						},
+// 						rsvp_id: rsvp.rsvp_id
+// 					});
+// 				}
+// 			});
+
+// 			return rsvps;
+// 		}
+
 // 		var model = dataMeetup.results[0];
-// 		model.rsvps = dataMeetupRsvps.results.sort(MeetupRaffler.sorter('member.name', false, function(a){return a.toUpperCase()}));
+// 		var rsvps = explodeTheGuests(dataMeetupRsvps.results);
+// 		model.rsvps = rsvps.sort(MeetupRaffler.sorter('member.name', false, function(a){return a.toUpperCase()}));
 // 		return model; 
 // 	}
 // })
@@ -52,5 +73,20 @@ App.MeetupRoute = Ember.Route.extend(Ember.SimpleAuth.AuthenticatedRouteMixin, {
 			model.rsvps = rsvps.sort(MeetupRaffler.sorter('member.name', false, function(a){return a.toUpperCase()}))
 			return model;
 		});
+	}
+});
+
+App.MeetupController = Ember.ObjectController.extend({
+	actions: {
+		getRandomMember: function() {
+			var rsvps = this.get('model.rsvps');
+			var min = 0;
+			var max = rsvps.length - 1;
+			var memberIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+			var memberName = rsvps[memberIndex].member.name;
+			//alert('Member ' + memberName + " has won!");
+			MeetupRaffler.Notifier.notify('success', 'Member ' + memberName + " has won!");
+			return false;
+		}
 	}
 });
