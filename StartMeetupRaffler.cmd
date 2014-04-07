bb8cbd72b8af768531767bdd1c1606a9adf97550
Marcus Bristol <marcus.bristol@rededge.co.nz>
2014-04-07 18:53:11 +1200

    Added startup script

---
+++ StartMeetupRaffler.cmd
@@ -0,0 +1,5 @@
+@echo off
+cd C:\Marcus\Cloud\Dropbox\Shared Folders\RedEdge\Projects\MeetupRaffle\EmberJS
+start node scripts\web-server.js
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8002/app/index.html
+pause
