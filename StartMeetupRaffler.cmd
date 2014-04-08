da85268dfe02cf1f5c5ea92911ded995819bfada
Marcus Bristol <marcus.bristol@rededge.co.nz>
2014-04-08 23:02:10 +1200

    Removed user specific path

--- StartMeetupRaffler.cmd
+++ StartMeetupRaffler.cmd
@@ -1,5 +1,3 @@
 @echo off
-cd C:\Marcus\Cloud\Dropbox\Shared Folders\RedEdge\Projects\MeetupRaffle\EmberJS
 start node scripts\web-server.js
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8002/app/index.html
-pause
